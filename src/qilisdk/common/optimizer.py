b0f845969b9c9220ad3efc4998805ac0edfbe2e0
amirazzam2000 <ameerazzam123@gmail.com>
2025-02-24 10:48:53 +0100

    integrating optimizer

--- src/qilisdk/common/optimizer.py
+++ src/qilisdk/common/optimizer.py
@@ -13,4 +13,72 @@
 # limitations under the License.
 
 
-class Optimizer: ...
+from typing import Any, Callable
+
+from scipy import optimize as sci_opt
+
+
+class Optimizer:
+    def __init__(self) -> None:
+        self._optimal_parameters: list[float] = []
+
+    @property
+    def optimal_parameters(self) -> list[float]:
+        if len(self._optimal_parameters) == 0:
+            raise ValueError("No function has been optimized yet")
+        return self._optimal_parameters
+
+    def optimize(
+        self, cost_function: Callable[[list[float]], float], init_parameters: list[float], **kwargs: dict[str, Any]
+    ) -> list[float]:
+        """optimize the cost function and return the optimal parameters.
+
+        Args:
+            cost_function (Callable[[list[float]], float]): a function that takes in a list of parameters and returns
+            the cost.
+            TODO: change the cost function for a model instance.
+            init_parameters (list[float]): the list of initial parameters. Note: the length of this list determines
+            the number of parameters the optimizer will consider.
+
+        Raises:
+            NotImplementedError: because this is an abstract class.
+
+        Returns:
+            list[float]: the optimal set of parameters that minimize the cost function.
+        """
+        raise NotImplementedError
+
+
+class GradientDecent(Optimizer):
+
+    def optimize(
+        self,
+        cost_function: Callable[[list[float]], float],
+        init_parameters: list[float],
+        **kwargs: dict[str, Any],
+    ) -> list[float]:
+        """optimize the cost function and return the optimal parameters.
+
+        Args:
+            cost_function (Callable[[list[float]], float]): a function that takes in a list of parameters and returns
+            the cost.
+            TODO: change the cost function for a model instance.
+            init_parameters (list[float]): the list of initial parameters. Note: the length of this list determines
+            the number of parameters the optimizer will consider.
+
+        Optional Args:
+            method (str | Callable): one of the methods offered by scipy optimize
+            cost_function_params (tuple(Any, ...)): a tuple of extra parameters to be passed to the cost function.
+            Note: any other parameter specified in scipy optimize can be used in this method.
+            (https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html)
+
+        Returns:
+            list[float]: the optimal set of parameters that minimize the cost function.
+        """
+        method = kwargs.pop("method", "Powell")
+        cost_function_params = kwargs.pop("cost_function_params", ())
+
+        res = sci_opt.minimize(cost_function, args=cost_function_params, method=method, x0=init_parameters, **kwargs)
+        self._optimal_parameters = res.x
+
+        return res
