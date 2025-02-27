42daa3eaa0b44721712c9826b9dc4201cedbfdeb
Vyron Vasileiads <hi@fedonman.com>
2025-02-27 11:30:47 +0100

    Improve tests, add changelog entry

--- tests/analog/test_hamiltionian.py
+++ tests/analog/test_hamiltionian.py
@@ -7,7 +7,7 @@ from qilisdk.analog import Hamiltonian, I, X, Y, Z
 #  ADDITION TESTS
 # -----------------------------
 @pytest.mark.parametrize(
-    ("hamiltonian", "expected_ham"),
+    ("hamiltonian", "expected_hamiltonian"),
     [
         (Z(0) + Z(1), Z(0) + Z(1)),
         (Z(0) + Z(0), 2 * Z(0)),
@@ -25,15 +25,15 @@ from qilisdk.analog import Hamiltonian, I, X, Y, Z
         (1 + Z(0) + 3 + Z(1) + Z(0) + 2j, (4 + 2j) + 2 * Z(0) + Z(1)),
     ],
 )
-def test_addition(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
-    assert hamiltonian == expected_ham
+def test_addition(hamiltonian: Hamiltonian, expected_hamiltonian: Hamiltonian):
+    assert hamiltonian == expected_hamiltonian
 
 
 # -----------------------------
 #  SUBTRACTION TESTS
 # -----------------------------
 @pytest.mark.parametrize(
-    ("hamiltonian", "expected_ham"),
+    ("hamiltonian", "expected_hamiltonian"),
     [
         (Z(0) - Z(1), Z(0) - Z(1)),
         (Z(0) - Z(0), 0),  # i.e. 0
@@ -46,15 +46,15 @@ def test_addition(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
         (1 + Z(0) - 3 + Z(1) + Z(0) - 2j, (-2 - 2j) + 2 * Z(0) + Z(1)),
     ],
 )
-def test_subtraction(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
-    assert hamiltonian == expected_ham
+def test_subtraction(hamiltonian: Hamiltonian, expected_hamiltonian: Hamiltonian):
+    assert hamiltonian == expected_hamiltonian
 
 
 # -----------------------------
 #  MULTIPLICATION TESTS
 # -----------------------------
 @pytest.mark.parametrize(
-    ("hamiltonian", "expected_ham"),
+    ("hamiltonian", "expected_hamiltonian"),
     [
         (Z(0) * Z(0), Hamiltonian({(I(0),): 1})),
         (Z(0) * X(0), 1j * Y(0)),
@@ -79,15 +79,15 @@ def test_subtraction(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
         ),
     ],
 )
-def test_multiplication(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
-    assert hamiltonian == expected_ham
+def test_multiplication(hamiltonian: Hamiltonian, expected_hamiltonian: Hamiltonian):
+    assert hamiltonian == expected_hamiltonian
 
 
 # -----------------------------
 #  DIVISION TESTS
 # -----------------------------
 @pytest.mark.parametrize(
-    ("hamiltonian", "expected_ham"),
+    ("hamiltonian", "expected_hamiltonian"),
     [
         (Z(0) / 2, 0.5 * Z(0)),
         ((Z(0) + Z(1)) / 2, 0.5 * Z(0) + 0.5 * Z(1)),
@@ -96,5 +96,25 @@ def test_multiplication(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
         ((Z(0) + X(1)) / 1j, -1j * Z(0) + -1j * X(1)),
     ],
 )
-def test_division(hamiltonian: Hamiltonian, expected_ham: Hamiltonian):
-    assert hamiltonian == expected_ham
+def test_division(hamiltonian: Hamiltonian, expected_hamiltonian: Hamiltonian):
+    assert hamiltonian == expected_hamiltonian
+
+
+# -----------------------------
+#  __STR__
+# -----------------------------
+@pytest.mark.parametrize(
+    ("hamiltonian", "expected_str"),
+    [
+        (Z(0) / 2, "0.5 Z(0)"),
+        (Z(0) + Z(1), "Z(0) + Z(1)"),
+        (Z(0) + 2, "2 + Z(0)"),
+        (Z(0) + 2j, "2j + Z(0)"),
+        (Z(0) - 2, "-2 + Z(0)"),
+        (Z(0) - 2j, "-2j + Z(0)"),
+        (Z(0) - 3 + 2j, "(-3+2j) + Z(0)"),
+        (Z(0) - 3 + 2j - 2 * Z(0), "(-3+2j) - Z(0)"),
+    ],
+)
+def test_str(hamiltonian: Hamiltonian, expected_str: str):
+    assert str(hamiltonian) == expected_str
