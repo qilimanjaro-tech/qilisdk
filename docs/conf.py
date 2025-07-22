13c4a614e23ae887546b64564f474d15b2df541d
Vyron Vasileiadis <hi@fedonman.com>
2025-07-22 16:55:41 +0200

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-#from sphinxawesome_theme.postprocess import Icons  # pylint: disable=import-error
+import sys
+from pathlib import Path
 
-project = 'qiliSDK documentation'
+# if your package root is one level up in `src/`
+sys.path.insert(0, Path("../src").resolve())
+
+# from sphinxawesome_theme.postprocess import Icons  # pylint: disable=import-error
+
+project = 'QiliSDK'
 copyright = '2025, Qilimanjaro Quantum Tech'
 author = 'Qilimanjaro Quantum Tech'
 
@@ -23,6 +29,9 @@ extensions = [
     "sphinx_mdinclude",  # allows the mdinclude directive to add Markdown files
     "sphinx.ext.napoleon",  # converts Google docstrings into rst
     "sphinx_automodapi.automodapi",
+    "autoapi.extension",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ['_templates']
@@ -39,13 +48,39 @@ automodapi_toctreedirnm = "code/api"  # location where the automodapi rst files 
 
 autoclass_content = "class"  # only show class docstrings (hide init docstrings)
 
+# -- AutoAPI configuration ----------------------------------------------
+
+# tell autoapi to document Python code
+autoapi_type = "python"
+
+# where your package source lives (relative to the docs folder)
+autoapi_dirs = ["../src/qilisdk"]
+
+# where to output the generated rst files
+autoapi_root = "code/api"
+
+# disable automatic insertion into your toctrees
+# (you'll add the main entry manually in rst)
+autoapi_add_toctree_entry = True
+
+# Optional tweaks:
+# autoapi_keep_files = True       # keep the generated .rst files in your repo
+# autoapi_member_order = "bysource"  # or "alphabetical"
+# autoapi_options = [
+#     "members",
+#     "undoc-members",
+#     "show-inheritance",
+#     "show-module-summary",
+# ]
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_title = project
-#html_permalinks_icon = Icons.permalinks_icon
+# html_permalinks_icon = Icons.permalinks_icon
 html_favicon = "_static/q_light.jpeg"
-#html_baseurl = "https://docs.qilimanjaro.tech/"
+# html_baseurl = "https://docs.qilimanjaro.tech/"
 html_theme = "furo"
 html_theme_options = {
     "light_logo": "Logo_black.png",
