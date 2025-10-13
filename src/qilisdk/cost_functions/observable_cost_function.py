f207afa1d89e59c224000af2035ff829160616de
amirazzam2000 <ameerazzam123@gmail.com>
2025-10-13 16:55:32 +0200

    adding extra docs

--- src/qilisdk/cost_functions/observable_cost_function.py
+++ src/qilisdk/cost_functions/observable_cost_function.py
@@ -28,7 +28,26 @@ if TYPE_CHECKING:
 
 
 class ObservableCostFunction(CostFunction):
+    """
+    Compute costs by taking expectation values of observables.
+
+    Example:
+        .. code-block:: python
+
+            from qilisdk.analog.hamiltonian import Z
+            from qilisdk.cost_functions import ObservableCostFunction
+
+            cost_fn = ObservableCostFunction(Z(0))
+    """
+
     def __init__(self, observable: QTensor | Hamiltonian | PauliOperator) -> None:
+        """
+        Args:
+            observable (QTensor | Hamiltonian | PauliOperator): Quantum observable whose expectation value defines the cost.
+
+        Raises:
+            ValueError: If the provided observable type is unsupported.
+        """
         super().__init__()
         if isinstance(observable, QTensor):
             self._observable = observable
@@ -43,9 +62,18 @@ class ObservableCostFunction(CostFunction):
 
     @property
     def observable(self) -> QTensor:
+        """Return the observable in ``QTensor`` form."""
         return self._observable
 
     def _compute_cost_time_evolution(self, results: TimeEvolutionResult) -> Number:
+        """
+        Returns:
+            Number: the expectation value of ``observable`` given a final quantum state.
+
+        Raises:
+            ValueError: If the final state is not provided in the results.
+
+        """
         if results.final_state is None:
             raise ValueError(
                 "can't compute cost using Observables from time evolution results when the state is not provided."
@@ -56,6 +84,15 @@ class ObservableCostFunction(CostFunction):
         return total_cost
 
     def _compute_cost_sampling(self, results: SamplingResult) -> Number:
+        """
+        Estimate the observable expectation value using measured bitstring probabilities.
+
+        Returns:
+            Number: the expectation value of ``observable`` estimated from sampled bitstrings.
+
+        Raises:
+            ValueError: If the number of qubits in the observable does not match the sample size
+        """
         total_cost = complex(0.0)
         nqubits = self._observable.nqubits
         for sample, prob in results.get_probabilities():
