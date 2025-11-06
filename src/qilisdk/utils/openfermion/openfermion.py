84e04c7ab9ee6149b41b141b31f5e6990511fe21
amirazzam2000 <ameerazzam123@gmail.com>
2025-11-06 10:00:46 +0100

    adding qilisdk <-> openfermion translation util

---
+++ src/qilisdk/utils/openfermion/openfermion.py
@@ -0,0 +1,37 @@
+# Copyright 2025 Qilimanjaro Quantum Tech
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from openfermion import QubitOperator
+
+from qilisdk.analog import Hamiltonian, PauliX, PauliY, PauliZ
+
+
+def openfermion_to_qili(of_ham: QubitOperator) -> Hamiltonian:
+    pauli_map = {"X": PauliX, "Y": PauliY, "Z": PauliZ}
+
+    return Hamiltonian({tuple(pauli_map[op](q) for q, op in term): coeff for term, coeff in of_ham.terms.items()})
+
+
+def qili_to_openfermion(qili_ham: Hamiltonian) -> QubitOperator:
+    of_ham = QubitOperator()
+
+    for coeff, terms in qili_ham:
+        of_term = ""
+        for t in terms:
+            of_term += str(t).replace("(", "").replace(")", "") + " "
+        of_term = of_term.rstrip()
+
+        of_ham += QubitOperator(of_term, coeff)
+
+    return of_ham
