6ec426337e0f8f04c3e57101fbbfe8194a4f11d0
Vyron Vasileiadis <hi@fedonman.com>
2026-04-30 14:24:41 +0300

    remove class check

--- src/qilisdk/digital/gates.py
+++ src/qilisdk/digital/gates.py
@@ -452,12 +452,6 @@ class Modified(Gate, Generic[TBasicGate]):
 @yaml.register_class
 class Controlled(Modified[TBasicGate]):
     def __init__(self, *control_qubits: int, basic_gate: TBasicGate | Controlled[TBasicGate]) -> None:
-
-        # Make sure it's an instance of the class and not the class itself
-        # i.e. Z(0).controlled(1) not Z.controlled(0, 1)
-        if not isinstance(basic_gate, Gate):
-            raise ValueError("basic_gate must be an instance of a Gate, not the class itself.")
-
         # If doing Controlled of another Controlled, combine into one with all control qubits.
         if isinstance(basic_gate, Controlled) and isinstance(basic_gate.basic_gate, Gate):
             control_qubits += basic_gate.control_qubits
