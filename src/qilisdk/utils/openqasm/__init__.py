5ec55c400b7d4fe0e46db8190eb651b64a084083
amirazzam2000 <ameerazzam123@gmail.com>
2026-05-18 15:21:14 +0200

    adding qir and moving openqasm to be an optional dependency

---
+++ src/qilisdk/utils/openqasm/__init__.py
@@ -0,0 +1,52 @@
+# Copyright 2026 Qilimanjaro Quantum Tech
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+
+from qilisdk._optionals import (
+    DependencyGroup,
+    ImportedFeature,
+    OptionalFeature,
+    RequirementMode,
+    Symbol,
+    import_optional_dependencies,
+)
+
+__all__ = []
+
+OPTIONAL_FEATURES: list[OptionalFeature] = [
+    OptionalFeature(
+        name="pyqir",
+        mode=RequirementMode.ALL,
+        dependency_groups=[DependencyGroup(dists=["pyqir"], extra="qir")],
+        symbols=[
+            Symbol(path="qilisdk.utils.openqasm.openqasm2", name="to_qasm2"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm2", name="to_qasm2_file"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm2", name="from_qasm2"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm2", name="from_qasm2_file"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm3", name="to_qasm3"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm3", name="to_qasm3_file"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm3", name="from_qasm3"),
+            Symbol(path="qilisdk.utils.openqasm.openqasm3", name="from_qasm3_file"),
+        ],
+    ),
+]
+
+current_module = sys.modules[__name__]
+
+# Dynamically import (or stub) each feature's symbols and attach them
+for feature in OPTIONAL_FEATURES:
+    imported_feature: ImportedFeature = import_optional_dependencies(feature)
+    for symbol_name, symbol_obj in imported_feature.symbols.items():
+        setattr(current_module, symbol_name, symbol_obj)
+        __all__ += [symbol_name]  # noqa: PLE0604
