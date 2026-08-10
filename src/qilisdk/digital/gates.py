c900ac454a3310786f631ab3e24307bdb1f6322f
Lumorti <lbmortimer1998@gmail.com>
2026-08-10 12:38:15 +0200

    fix and tests

--- src/qilisdk/digital/gates.py
+++ src/qilisdk/digital/gates.py
@@ -250,11 +250,14 @@ class Gate(Parameterizable, ABC):
         return (
             self.name == other.name
             and self.qubits == other.qubits
-            and np.allclose(self.get_parameter_values(), other.get_parameter_values())
+            and np.allclose(self.get_parameter_values(), other.get_parameter_values(), atol=get_settings().atol)
         )
 
     def __hash__(self) -> int:
-        return qili_hash((self.name, self.qubits, self.matrix, tuple(self.get_parameters())))
+        atol = get_settings().atol
+        _PARAMETER_HASH_DECIMALS = int(-np.log10(atol))
+        rounded_values = tuple(round(value, _PARAMETER_HASH_DECIMALS) for value in self.get_parameter_values())
+        return qili_hash((self.name, self.qubits, rounded_values))
 
 
 @yaml.register_class
