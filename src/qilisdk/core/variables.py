11013bf3affbdc6f8df1195e3198885bed6a9106
Lumorti <lbmortimer1998@gmail.com>
2026-08-19 13:00:06 +0200

    made all encoding methods more consistent

--- src/qilisdk/core/variables.py
+++ src/qilisdk/core/variables.py
@@ -383,12 +383,11 @@ class Bitwise(Encoding):
         if var.domain is Domain.REAL:
             bounds = (bounds[0] / precision, bounds[1] / precision)
 
-        abs_bound = np.abs(bounds[1] - bounds[0])
-        n_binary = int(np.floor(np.log2(abs_bound if abs_bound != 0 else 1)))
-        binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary + 1)]
+        n_binary = Bitwise.num_binary_equivalent(var, precision)
+        binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary)]
 
-        term = sum(2**i * binary_vars[i] for i in range(n_binary))
-        term += (np.abs(bounds[1] - bounds[0]) + 1 - 2**n_binary) * binary_vars[-1]
+        term = sum(2**i * binary_vars[i] for i in range(n_binary - 1))
+        term += (np.abs(bounds[1] - bounds[0]) + 1 - 2 ** (n_binary - 1)) * binary_vars[-1]
         term += bounds[0]
         return term * var.precision if var.domain is Domain.REAL else term
 
@@ -431,7 +430,7 @@ class Bitwise(Encoding):
         if var.domain is Domain.REAL:
             bounds = (bounds[0] / precision, bounds[1] / precision)
 
-        # Vars with fixed value should be represented by a single (unused) binary variable
+        # Fixed vars for now are represented by a single (unused) binary variable
         if bounds[1] - bounds[0] == 0:
             return 1
 
@@ -483,7 +482,7 @@ class OneHot(Encoding):
         if var.domain is Domain.REAL:
             bounds = (bounds[0] / precision, bounds[1] / precision)
 
-        n_binary = int(np.abs(bounds[1] - bounds[0])) + 1
+        n_binary = OneHot.num_binary_equivalent(var, precision)
 
         binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary)]
 
@@ -527,11 +526,7 @@ class OneHot(Encoding):
 
     @staticmethod
     def encoding_constraint(var: Variable, precision: float = 1e-2) -> ComparisonTerm:
-        bounds = var.bounds
-        if var.domain is Domain.REAL:
-            bounds = (bounds[0] / precision, bounds[1] / precision)
-
-        n_binary = int(np.abs(bounds[1] - bounds[0])) + 1
+        n_binary = OneHot.num_binary_equivalent(var, precision)
 
         binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary)]
         return ComparisonTerm(lhs=sum(binary_vars), rhs=1, operation=ComparisonOperation.EQ)
@@ -583,7 +578,7 @@ class DomainWall(Encoding):
         if var.domain is Domain.REAL:
             bounds = (bounds[0] / precision, bounds[1] / precision)
 
-        n_binary = int(np.abs(bounds[1] - bounds[0]))
+        n_binary = DomainWall.num_binary_equivalent(var, precision)
 
         binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary)]
 
@@ -626,11 +621,7 @@ class DomainWall(Encoding):
 
     @staticmethod
     def encoding_constraint(var: Variable, precision: float = 1e-2) -> ComparisonTerm:
-        bounds = var.bounds
-        if var.domain is Domain.REAL:
-            bounds = (bounds[0] / precision, bounds[1] / precision)
-
-        n_binary = int(np.abs(bounds[1] - bounds[0]))
+        n_binary = DomainWall.num_binary_equivalent(var, precision)
 
         binary_vars = [BinaryVariable(var.label + f"({i})") for i in range(n_binary)]
         return ComparisonTerm(
